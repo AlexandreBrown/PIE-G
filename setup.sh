d251d9e6e2d50f551c995073a815d9aca203d538
AlexandreBrown <alexandrebrown97@gmail.com>
2024-11-04 17:20:33 -0500

    Added missing not done tensor

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 
 pip install -e ./baselines/pie-g/
 
-# wget http://data.csail.mit.edu/places/places365/places365standard_easyformat.tar -P ./baselines/pie-g/data/
-
-# tar -xvf ./baselines/pie-g/data/places365standard_easyformat.tar -C baselines/pie-g/data/
-
-# rm ./baselines/pie-g/data/places365standard_easyformat.tar
+if [ ! -d "./baselines/data/places365_standard" ]; then
+    wget http://data.csail.mit.edu/places/places365/places365standard_easyformat.tar -P ./baselines/data/
+    tar -xvf ./baselines/data/places365standard_easyformat.tar -C baselines/data/
+    rm ./baselines/data/places365standard_easyformat.tar
+else
+    echo "The folder 'places365_standard' already exists. Skipping download and extraction."
+fi
